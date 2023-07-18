12505b460ba78a98844ce8c2b01ef2a93d73a067
Test IT <50841235+testit-owner@users.noreply.github.com>
2023-07-18 14:11:38 +0300

    Added selenide (#9)
    * Added Selenide example
    
    ---------
    
    Co-authored-by: Dmitry.Gridnev <dmitry.gridnev@testit.software>

---
+++ selenide-gradle-junit5/src/test/java/ExampleTests.java
@@ -0,0 +1,45 @@
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.junit5.TextReportExtension;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import ru.testit.selenide.SelenideListener;
+
+import static com.codeborne.selenide.Condition.text;
+
+@ExtendWith(TextReportExtension.class)
+public class ExampleTests {
+
+    private Steps steps;
+
+    @BeforeEach
+    public void setUp() {
+        SelenideLogger.addListener(
+                "TmsSelenide",
+                new SelenideListener()
+                        .saveScreenshots(true)
+                        .savePageSource(true)
+                        .includeSelenideSteps(true));
+
+        steps = new Steps();
+    }
+
+    @Test
+    public void TestFailed() {
+        steps.openPage();
+
+        SelenideElement searchField = steps.getElementByXpath("//h1[contains(@class,\"title\")]");
+
+        searchField.shouldHave(text("Система для управления тестированием"));
+    }
+
+    @Test
+    public void TestSuccess() {
+        steps.openPage();
+
+        SelenideElement searchField = steps.getElementByXpath("//h1[contains(@class,\"title\")]");
+
+        searchField.shouldHave(text("Система управления тестированием"));
+    }
+}
