842cfdc732c275ea709633966afc10d58b764375
pavel.butuzov <pavel.butuzov@testit.software>
2025-04-02 09:17:30 +0300

    feat: TMS-32116: add the example for cucumber7-maven-testng.

---
+++ cucumber7-maven-testng/src/test/java/examples/CucumberTest.java
@@ -0,0 +1,18 @@
+package examples;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
+
+@Test
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "examples",
+        plugin = {
+                "ru.testit.listener.BaseCucumber7Listener",
+                "progress",
+                "summary"
+        }
+)
+public class CucumberTest extends AbstractTestNGCucumberTests {
+}
